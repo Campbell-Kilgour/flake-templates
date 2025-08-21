3b17fb7f1deab16c2c01d18d07cdbfbe78ab6f69
Campbell Kilgour <65951539+Campbell-Kilgour@users.noreply.github.com>
2025-08-21 09:40:40 +0930

    Fixed error in root flake

--- flake.nix
+++ flake.nix
@@ -1,17 +1,15 @@
 {
   description = "My collection of Nix Flakes for easily creating Nix Shell environments for new projects.";
 
-  outputs = {selfm nixpkgs }: {
-    outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs }: {
     templates.fish = {
       path = ./fish;
       description = "A generi shell environment setup with Fish";
     };
 
     templates.rust = {
-        path = ./rust;
-        description = "A nix shell environment ready for Rust development."
-      };
+      path = ./rust;
+      description = "A nix shell environment ready for Rust development."
+    };
   };
-  }
 }
